b57426f9842f0d5c87019a1e6cfa49296d9107fa
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2023-02-17 16:57:10 +0800

    feat: add dprint config

---
+++ packages/dprint-config/index.js
@@ -0,0 +1,10 @@
+const fs = require('node:fs')
+const jsonc = require('jsonc-parser')
+
+try {
+  fs.mkdirSync('dist')
+} catch (_e) {}
+
+const conf = fs.readFileSync('dprint.json', 'utf8').toString()
+const out = fs.openSync('dist/dprint.json', 'w')
+fs.writeFileSync(out, JSON.stringify(jsonc.parse(conf), undefined, 2))
