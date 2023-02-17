403f7e8f66a34f15ca008f5d7019afa8018aa78d
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2023-02-17 17:20:42 +0800

    feat(eslint-config): update finalOverrides.js from antfu

--- packages/eslint-config/finalOverrides.js
+++ packages/eslint-config/finalOverrides.js
@@ -16,7 +16,7 @@ module.exports = [
   },
 
   // These suffix also matches more broad '*.js' and '*.ts', so need to be overridden last
-  // From: https://github.com/antfu/eslint-config/blob/b34da44baf49180b94b2518e2a07db66410bcbce/packages/basic/index.js
+  // https://github.com/antfu/eslint-config/blob/37a4d762c388220b97790c5b7b12618426bd2fee/packages/basic/index.js
   {
     files: ['*.d.ts'],
     rules: {
@@ -33,6 +33,7 @@ module.exports = [
     files: ['*.test.ts', '*.test.js', '*.spec.ts', '*.spec.js'],
     rules: {
       'no-unused-expressions': 'off',
+      'no-only-tests/no-only-tests': 'error',
     },
   },
   {
@@ -44,7 +45,10 @@ module.exports = [
       '@typescript-eslint/no-use-before-define': 'off',
       '@typescript-eslint/no-var-requires': 'off',
       '@typescript-eslint/comma-dangle': 'off',
+      '@typescript-eslint/consistent-type-imports': 'off',
       'import/no-unresolved': 'off',
+      'unused-imports/no-unused-imports': 'off',
+      'unused-imports/no-unused-vars': 'off',
       'no-alert': 'off',
       'no-console': 'off',
       'no-restricted-imports': 'off',
