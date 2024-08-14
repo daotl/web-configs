c7a412cfe7c76e15fe661d26aede41618d0ae2cd
Nex Zhu <nexzhu@gmail.com>
2024-08-14 14:06:13 +0800

    style(all): format

--- packages/dprint-config/index.js
+++ packages/dprint-config/index.js
@@ -1,9 +1,10 @@
 const fs = require('node:fs')
 const jsonc = require('jsonc-parser')
 
-try {
-  fs.mkdirSync('dist')
-} catch (_e) {}
+// try {
+fs.mkdirSync('dist')
+// } catch (_e) {
+// }
 
 const conf = fs.readFileSync('dprint.json', 'utf8').toString()
 const out = fs.openSync('dist/dprint.json', 'w')
